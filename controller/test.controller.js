24095d07ae8a30f7ac081bac104e64a5e635cbd1
Jyoti Rajpal <jyoti.r@webvillee.in>
2023-04-17 17:26:44 +0530

    route change

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -2,7 +2,11 @@ greetings = function(req,res){
     res.json({status:200, success:true, message: 'Welcome to Webvillee'});
 };
 
+greetings_1 = function(req,res){
+    res.json({status:200, success:true, message: 'Welcome to Jenkins'});
+};
 
 module.exports = {
     greetings, 
+    greetings_1
 }
--- router/my.router.js
+++ router/my.router.js
@@ -2,6 +2,7 @@ var router = require("express").Router();
 var testcontroller = require('../controller/test.controller');
 
 router.get('/greetings', testcontroller.greetings);
+router.get('/greetings_1', testcontroller.greetings_1);
 
 
 
